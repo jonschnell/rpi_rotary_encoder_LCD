b98b0bd949d4b38a9ab265fb402afe0897642677
Jon Schnell <jonathonrschnell@gmail.com>
2022-05-16 03:30:34 -0500

    encoder now supports click interrupt

--- encoder.py
+++ encoder.py
@@ -8,22 +8,44 @@ minPage = 0
 
 class Encoder:
 
-    def __init__(self, leftPin, rightPin, callback=None):
+    def __init__(self, leftPin, rightPin, centerPin, callback=None):
         self.leftPin = leftPin
         self.rightPin = rightPin
+        self.centerPin = centerPin
         self.value = 0
         self.state = '00'
         self.direction = None
+        self.click = 0
+        self.p3last = 1
         self.callback = callback
         GPIO.setup(self.leftPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.rightPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.centerPin, GPIO.IN)
         GPIO.add_event_detect(self.leftPin, GPIO.BOTH, callback=self.transitionOccurred)  
-        GPIO.add_event_detect(self.rightPin, GPIO.BOTH, callback=self.transitionOccurred)  
+        GPIO.add_event_detect(self.rightPin, GPIO.BOTH, callback=self.transitionOccurred)
+        GPIO.add_event_detect(self.centerPin, GPIO.BOTH, callback=self.transitionOccurred)
 
     def transitionOccurred(self, channel):
         p1 = GPIO.input(self.leftPin)
         p2 = GPIO.input(self.rightPin)
+        p3 = GPIO.input(self.centerPin)
         newState = "{}{}".format(p1, p2)
+
+        if self.p3last == 1:
+            if p3 == 1:
+                self.click = 0
+            elif p3 == 0:
+                self.click = 1
+                if self.callback is not None:
+                    self.callback(self.value, self.direction, self.click)
+
+        if self.p3last == 0:
+            if p3 == 0:
+                self.click = 1
+            elif p3 == 1:
+                self.click = 0
+                if self.callback is not None:
+                    self.callback(self.value, self.direction, self.click)
 
         if self.state == "00": # Resting position
             if newState == "01": # Turned right 1
@@ -39,7 +61,7 @@ class Encoder:
                     if self.value > minPage:
                         self.value = self.value - 1
                     if self.callback is not None:
-                        self.callback(self.value, self.direction)
+                        self.callback(self.value, self.direction, self.click)
 
         elif self.state == "10": # R3 or L1
             if newState == "11": # Turned left 1
@@ -49,9 +71,9 @@ class Encoder:
                     if self.value < maxPage:
                         self.value = self.value + 1
                     if self.callback is not None:
-                        self.callback(self.value, self.direction)
+                        self.callback(self.value, self.direction, self.click)
 
-        else: # self.state == "11"
+        elif self.state == "11":
             if newState == "01": # Turned left 1
                 self.direction = "L"
             elif newState == "10": # Turned right 1
@@ -61,14 +83,15 @@ class Encoder:
                     if self.value > minPage:
                         self.value = self.value - 1
                     if self.callback is not None:
-                        self.callback(self.value, self.direction)
+                        self.callback(self.value, self.direction, self.click)
                 elif self.direction == "R":
                     if self.value < maxPage:
                         self.value = self.value + 1
                     if self.callback is not None:
-                        self.callback(self.value, self.direction)
-                
+                        self.callback(self.value, self.direction, self.click)
+
         self.state = newState
+        self.p3last = p3
 
     def getValue(self):
         return self.value
--- interface.py
+++ interface.py
@@ -5,31 +5,37 @@ from RPLCD.i2c import CharLCD
 
 lcd = CharLCD('PCF8574', 0x27)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
 
 version = "PWNB v0.0"
 
-def pageSelect(value, direction):
-    print("PAGE: {}, DIRECTION: {}".format(value, direction))
-    #toPrint = "PAGE: " + str(value)
+def pageSelect(value, direction, click):
+    print("PAGE: {}, DIRECTION: {}, CLICK: {}".format(value, direction, click))
+
     lcd.clear()
-    #lcd.write_string(toPrint)
+
     if value == 0:
         lcd.write_string(version)
     elif value == 1:
-        wifi()
+        lcd.write_string("WIFI")
+        if click == 1:
+            wifi()
     elif value == 2:
-        bluetooth()
+        lcd.write_string("BLUETOOTH")
+        if click == 1:
+            bluetooth()
     elif value == 3:
-        nfc()
+        lcd.write_string("NFC")
+        if click == 1:
+            nfc()
 
 
-e1 = Encoder(18, 24, pageSelect)
+
+e1 = Encoder(18, 24, 23, pageSelect)
 
 lcd.write_string(version)
 
 def wifi():
-    lcd.write_string("WIFI")
+    lcd.write_string("WIFI") 
 
 def bluetooth():
     lcd.write_string("BLUETOOTH")
@@ -40,9 +46,7 @@ def nfc():
 try:
     while True:
         time.sleep(.25)
-        if GPIO.input(23) == 0:
-            print("click")
-        #print("Value is {}".format(e1.getValue()))
+
 except Exception:
     pass
 
